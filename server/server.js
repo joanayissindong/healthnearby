1b4476a65696376354cfe919b5e8fde56df48d9c
joanayissindong <wilfried11ndong@gmail.com>
2026-03-01 17:52:12 +0100

    modification for vercel backend infrastructure

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0',() => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
+// En local seulement
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 module.exports = app;
