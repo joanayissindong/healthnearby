9e0bae6e9fd1d99210bbe26298f4fbf2421ff867
joanayissindong <wilfried11ndong@gmail.com>
2026-03-03 13:41:36 +0100

    feat: add Vercel Analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
 
@@ -8,3 +9,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+inject();
